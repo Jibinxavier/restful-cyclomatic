af25550258d1e03d489ffd61ce8bd5d7be345b69
Jibin Xavier <xavierj@tcd.ie>
2017-11-25 17:38:23 +0000

    moved database contructs into new module

--- src/Lib.hs
+++ src/Lib.hs
@@ -26,6 +26,7 @@ import        Control.Distributed.Process.Node                   (initRemoteTabl
 import        Control.Monad
 import        Network.Transport.TCP                              (createTransport, defaultTCPParameters)
 import        PrimeFactors
+import        DatabaseConnector
 import        System.Environment                                 (getArgs,lookupEnv)
 import        System.Exit
 import        Data.List
@@ -46,12 +47,7 @@ import        Codec.Archive.Zip (extractFiles, withArchive, entryNames)
 -- import        Network.HTTP.Conduit
 import        Network.URI (parseURI)
 import        Argon
-import        Data.Text                    (pack, unpack)
-import        Control.Monad.Trans.Resource  
-import qualified Data.ByteString.Lazy         as L
-import        Database.MongoDB   
-import        Data.Aeson.TH
-import        Data.Bson.Generic 
+
 import Data.Traversable (traverse)
 import System.Directory.Tree (
     AnchoredDirTree(..), DirTree(..),
@@ -64,7 +60,7 @@ listFilesDirFiltered :: String -> IO ([DirTree FilePath])
 listFilesDirFiltered dir = do
   _:/tree <- readDirectoryWith return dir
   let res = flattenDir (filterDir myPred tree)
-  return [ x | x@(File _ _) <- res ]
+  return [ x | x@(File _ _) <- res ]  -- removing all Dir data types as they are empty
    
    
   --return ()do
@@ -241,51 +237,3 @@ someFunc = do
   --   liftIO $ threadDelay 2000000
 
 
- 
-withMongoDbConnection :: Action IO a -> IO a
-withMongoDbConnection act  = do
-  ip <- mongoDbIp
-  port <- mongoDbPort
-  database <- mongoDbDatabase
-  pipe <- connect (host ip)
-  ret <- runResourceT $ liftIO $ access pipe master (pack database) act
-  close pipe
-  return ret
-
--- | helper method to ensure we force extraction of all results
--- note how it is defined recursively - meaning that draincursor' calls itself.
--- the purpose is to iterate through all documents returned if the connection is
--- returning the documents in batch mode, meaning in batches of retruned results with more
--- to come on each call. The function recurses until there are no results left, building an
--- array of returned [Document]
-mongoDbIp :: IO String
-mongoDbIp = defEnv "MONGODB_IP" id "localhost" True
-
--- | The port number of the mongoDB database that devnostics-rest uses to store and access data
-mongoDbPort :: IO Integer
-mongoDbPort = defEnv "MONGODB_PORT" read 27017 False -- 27017 is the default mongodb port
-
--- | The name of the mongoDB database that devnostics-rest uses to store and access data
-mongoDbDatabase :: IO String
-mongoDbDatabase = defEnv "MONGODB_DATABASE" id "USEHASKELLDB" True
-
-drainCursor :: Cursor -> Action IO [Document]
-drainCursor cur = drainCursor' cur []
-  where
-    drainCursor' cur res  = do
-      batch <- nextBatch cur
-      if null batch
-        then return res
-        else drainCursor' cur (res ++ batch)
-defEnv :: Show a
-              => String        -- Environment Variable name
-              -> (String -> a)  -- function to process variable string (set as 'id' if not needed)
-              -> a             -- default value to use if environment variable is not set
-              -> Bool          -- True if we should warn if environment variable is not set
-              -> IO a
-defEnv env fn def doWarn = lookupEnv env >>= \ e -> case e of
-  Just s  -> return $ fn s
-  Nothing -> do
-    when doWarn (putStrLn $ "Environment variable: " ++ env ++
-                                  " is not set. Defaulting to " ++ (show def))
-    return def
